61e83596b808b39a766e056b18b2c98eb706aaac
Mukul <coolm454@gmail.com>
2017-11-24 11:50:05 +0530

    day-night animation is done

--- app/components/header.js
+++ app/components/header.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 
 import css from '../../index.css';
 
+import Wheather from './wheather';
+
+const hours = new Date().getHours();
+const isDayTime = hours > 5 && hours < 19;
+
 class Header extends Component {
-    render() {
-        return (
-            <div>
+    constructor(props){
+        super(props);
+        this.state = {
+            day:"disp",
+            night:"no-disp"
+        }
+    }
+
+
+     render() {
+        
+            return(
+                <div>
+                <div className={ isDayTime ? this.state.day : this.state.night }>
                 <div className="scene">
                 <img className="sun" src="https://i.imgur.com/MdwJyGi.png"/>
     <img className="cloud sml" src="http://www.dwuser.com/education/content/creating-keyframe-animations-with-css3/images/cloud_sml.png" width="70" height="36"/>
@@ -13,11 +29,25 @@ class Header extends Component {
     <img className="cloud lrg" src="http://www.dwuser.com/education/content/creating-keyframe-animations-with-css3/images/cloud_lrg.png" width="130" height="67"/>
     <img className="blades" src="http://www.dwuser.com/education/content/creating-keyframe-animations-with-css3/images/blades.png" width="120" height="120"/>
     <img className="stand" src="http://www.dwuser.com/education/content/creating-keyframe-animations-with-css3/images/stand.png" width="12" height="130"/>
-       <div className="green"> __</div>         
+       <div className="green"> __</div>    
+       
                  </div>
                  <h1 className="heading-text">Wheather App</h1>
+                 </div>
+                <div className={ isDayTime ? this.state.night : this.state.day }>
+                 <div className="bg-night">
+                 <img className="light" src="https://i.imgur.com/Y2jfB4W.png"/>
+                 <img className="moon" src="https://i.imgur.com/A1ODH3N.png"/>
+                 <img className="grey-cloud cloud sml" src="https://i.imgur.com/LM2CKD3.png"/>
+                 <img className="grey-cloud cloud med" src="https://i.imgur.com/Ek0u6bt.png"/>
+                 <img className="sky" src="https://i.imgur.com/ypxixf4.png"/>
+                 <div className="black"> __</div>
+                 </div>
+                 <h1 className="heading-text">Wheather App</h1>
+                 </div>  
             </div>
         );
+    
     }
 }
 
--- app/components/wheather.js
+++ app/components/wheather.js
@@ -1,37 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-    
-    //Snippet for adding Script tag.
-    // const url1 = 'https://js.maxmind.com/js/apis/geoip2/v2.1/geoip2.js';
-    // const loadScript = (url) => {
-    //     var script = document.createElement("script");
-    //     script.type = "text/javascript";
 
-    //     if(script.readyState){ //For IE
-    //         script.onreadystatechange = function(){
-    //             if(script.readyState == "loaded" ||
-    //          script.readyState == "completed"){
-    //              script.onreadystatechange = null;
-    //          }
-    //         };
-    //     }
-    //     else{ //Others Browsers
-    //         script.onload = function(){
-    //             console.log("Tag added and Script Loaded!");
-    //         };
-    //     }
-
-    //     script.src = url;
-    //     document.getElementsByTagName("head")[0].appendChild(script);
-    // }
-    // loadScript(url1);
-
-    //GeoIP
-    // const ipSuccess = (location) => {
-    //     console.log(location);
-    // };
-    // const ipError = (err) =>{
-    //     console.log(err);
-    // }
 
 class Wheather extends Component {
     constructor(props) {
